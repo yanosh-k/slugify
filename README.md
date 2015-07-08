56c4ab69786d8f7a7b6dbde36ced987249ff2b12
Yanosh Kunsh <yanosh.kunsh@gmail.com>
2015-07-08 10:48:48 +0300

    update readme with installation, usage and customization infomation; fix function name; move double hyphens replacment block just before the return

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# slugify
-A MySQL function for creating slugs from strings.
+﻿# Slugify
+Slugify is a MySQL function for creating slugs from strings.
+
+It replaces all space characters with dashes, converts the upper-case english letters to lower-case and may use a temporary character map table to make an additional replacements.
+
+Currently the slugify function includes a bulgarian( cyrillic ) letters map table. This means that for each bulgarin letter in the passed string an equvalent english letter will be used.
+
+# Installation
+From the command line:
+```
+mysql -h HOST -u USER -p DB_NAME < slugify_function.sql
+```
+Or directly copy the contents of the sql file and execute it in your favourite graphic interface for MySQL.
+
+
+# Usage
+```
+SELECT SLUGIFY('My DiRtY Text That Has All these characters !@#$@#$%#$ in it') AS slug;
+-- The result will be: my-dirty-text-that-has-all-these-characters-in-it
+```
+
+```
+SELECT SLUGIFY(table.field AS field) FROM table;
+```
+
+# Customizing the replacement map table
+In order to customize the replacement map all you need to do is replace line 36 from the sql file.
+The line contains a character -> replacement pair values in the form:
+`('а', 'a'), ('б', 'b'), ('в', 'v')...`
+where the first character is the value you want to replace and the second is the value you want it replaced with.
--- slugify_function.sql
+++ slugify_function.sql
@@ -1,4 +1,4 @@
-﻿DROP FUNCTION IF EXISTS `BG_SLUGIFY`;
+﻿DROP FUNCTION IF EXISTS `SLUGIFY`;
 DELIMITER ;;
 
 CREATE FUNCTION `SLUGIFY`(dirty_string VARCHAR(255))
@@ -12,22 +12,27 @@ BEGIN
 	DECLARE is_allowed TINYINT(1);
 	
 	SET new_string = '';
+    -- Replace all spaces and dots with a dash
 	SET dirty_string = REPLACE(TRIM(REPLACE(dirty_string, '.', '-')), ' ', '-');
+    -- Set the allowed characters to be any letter from the english alphabet, numbers or hyphens
 	SET allowed_chars = 'abcdefghijklmnopqrstuvwxyz0123456789-';
 	SET string_length = CHAR_LENGTH(dirty_string);
 	SET counter = 1;
-	
-	WHILE LOCATE('--', dirty_string) > 0 DO
-		SET dirty_string = REPLACE(dirty_string, '--', '-');
-	END WHILE;
-
+    
+    -- Crete a temporary table that will hold the characters replacement map
 	CREATE TEMPORARY TABLE IF NOT EXISTS slug_temp_table (bul_letter VARCHAR(5) NOT NULL, eng_equiv VARCHAR(5) NOT NULL);
-	SELECT COUNT(*) INTO temp_table_rows FROM slug_temp_table;
+	
+    -- Comment out the following block if you don't want to use the character table
+    SELECT COUNT(*) INTO temp_table_rows FROM slug_temp_table;
 	IF temp_table_rows = 0 THEN
-		INSERT INTO slug_temp_table (bul_letter, eng_equiv) VALUES ('а', 'a'), ('б', 'b'), ('в', 'v'), ('г', 'g'), ('д', 'd'), ('е', 'e'), ('ж', 'j'), ('з', 'z'), ('и', 'i'), ('й', 'j'), ('к', 'k'), ('л', 'l'), ('м', 'm'), ('н', 'n'), ('о', 'o'), ('п', 'p'), ('р', 'r'), ('с', 's'), ('т', 't'), ('у', 'u'), ('ф', 'f'), ('х', 'h'), ('ц', 'c'), ('ч', 'ch'), ('ш', 'sh'), ('щ', 'sht'), ('ъ', 'y'), ('ь', 'i'), ('ю', 'u'), ('я', 'ja');
+		INSERT INTO slug_temp_table (bul_letter, eng_equiv) VALUES 
+            -- Replace the following line with the your preferred character map
+            ('а', 'a'), ('б', 'b'), ('в', 'v'), ('г', 'g'), ('д', 'd'), ('е', 'e'), ('ж', 'j'), ('з', 'z'), ('и', 'i'), ('й', 'j'), ('к', 'k'), ('л', 'l'), ('м', 'm'), ('н', 'n'), ('о', 'o'), ('п', 'p'), ('р', 'r'), ('с', 's'), ('т', 't'), ('у', 'u'), ('ф', 'f'), ('х', 'h'), ('ц', 'c'), ('ч', 'ch'), ('ш', 'sh'), ('щ', 'sht'), ('ъ', 'y'), ('ь', 'i'), ('ю', 'u'), ('я', 'ja')
+        ;
 	END IF;
+    
 	
-	
+	-- Make the actual replacements character by character
 	WHILE counter <= string_length DO
 		SET current_char = SUBSTRING(dirty_string, counter, 1);
 		SET is_allowed = LOCATE(current_char, allowed_chars);
@@ -43,7 +48,13 @@ BEGIN
 		SET found_equiv = '';
 		SET counter = counter + 1;
 	END WHILE;
+    
+        -- Replace the double hyphens with a single hyphen
+	WHILE LOCATE('--', new_string) > 0 DO
+		SET new_string = REPLACE(new_string, '--', '-');
+	END WHILE;
 	
+    
 	RETURN new_string;
 
 END;;
